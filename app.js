63dfb46a6bb70aaac76b6cbff0a806395b847b7e
Jorge <jorgeedu7503@gmail.com>
2025-12-05 18:22:58 -0600

    Agregar nuevos archivos

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+
 const authRoutes = require('./src/routes/auth-routes');
 const recipeRoutes = require('./src/routes/recipe-routes');
 const userRoutes = require('./src/routes/user-routes'); // Vital para favoritos y admin
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
